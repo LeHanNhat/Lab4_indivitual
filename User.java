7ee29e6f8aa3f950c0004281d07bda3b78f5c725
quangtechno <quangdodang101002@gmail.com>
2023-11-11 14:05:09 +0700

    push

---
+++ User.java
@@ -0,0 +1,21 @@
+public class User{
+    private String LeaseDetail;
+    private String PropertyDetails;
+    private String LeaseStatus; 
+    private String LeaseTerms;
+    
+    public User(String leaseDetail, String propertyDetails, String leaseStatus, String leaseTerms) {
+        LeaseDetail = leaseDetail;
+        PropertyDetails = propertyDetails;
+        LeaseStatus = leaseStatus;
+        LeaseTerms = leaseTerms;
+    }
+    
+    public void createRentalContract(){
+
+    }
+    public void TerminateRentalContract(){
+
+    }
+
+}
