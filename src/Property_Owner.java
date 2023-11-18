5e49c2557f5c5f309890e2389e3381d4bae3c7e2
LeHanNhat <149213739+LeHanNhat@users.noreply.github.com>
2023-11-18 13:50:38 +0700

    update from lehannhat

--- src/Property_Owner.java
+++ src/Property_Owner.java
@@ -1,3 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 public class Property_Owner {
+    public  StringBuffer sb = new StringBuffer(); 
+    private ArrayList<Property> propertyOwned;
+    private String legalStatus;
+    private ArrayList<String> paymentHistory;
+
+    private Property property;
     
+    public Property_Owner(ArrayList<Property> propertyOwned, String legalStatus, ArrayList<String> paymentHistory) {
+        this.propertyOwned = propertyOwned;
+        this.legalStatus = legalStatus;
+        this.paymentHistory = paymentHistory;
+    }
+
+    public void ListProperty() {
+        for (int i = 0;i<this.propertyOwned.size();i++) {
+            sb.append(this.propertyOwned.get(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public void CreateProperty(String propertyID, Property property2) {
+        property.CreateProperty(propertyID,property2);
+        propertyOwned.add(property);
+    }
+
+    public void updatePropertyOwned(int id, Property property3) {
+        propertyOwned.get(id).UpgradeProperty(property3.getPropertyID(), property3.getDescription(), property3.getRentalRate());
+    }
+    public void DeleteProperty(int id,String propertyID){
+        propertyOwned.get(id).DeleteProperty(propertyID);
+    }
 }
